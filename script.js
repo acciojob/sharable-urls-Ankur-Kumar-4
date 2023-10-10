c0284d1638458b47c51a43bedd63df8a540d906a
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-10 12:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// your code here
+
+
+
+document.getElementById("button").addEventListener("click",()=>{
+
+let nameValue = document.getElementById("name").value;
+let yearValue = document.getElementById("year").value;
+
+	
+	if(nameValue!='' && yearValue!=''){
+                 document.getElementById("url").innerHTML=`
+                   https://localhost:8080/?name=${nameValue}&year=${yearValue}`
+	}
+	else if(nameValue!=''){
+		 document.getElementById("url").innerHTML=`
+                   https://localhost:8080/?name=${nameValue}`
+	}
+	else if(yearValue!=''){
+		 document.getElementById("url").innerHTML=`
+                   https://localhost:8080/?year=${yearValue}`
+	}
+	
+	
+});
